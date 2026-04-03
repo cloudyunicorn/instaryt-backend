3da50d978b2d199e16a79fd5e15571a1530a5b65
Rajat Singh <s.rajat55@gmail.com>
2026-04-03 14:09:55 +0530

    cors fix

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import post
 from app.routes import webhook
 
 app = FastAPI(title="Instagram Posting Service")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 
